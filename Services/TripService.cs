45fbd48be9acbcee2b618e8a0c07a70f2a8a1438
Keauk <keauk8@gmail.com>
2024-05-25 19:03:32 +0200

    Add base implementation stubs for trip and user service.

--- Services/TripService.cs
+++ Services/TripService.cs
@@ -1,6 +1,28 @@
-﻿namespace TravelBuddyAPI.Services
+﻿using TravelBuddyAPI.Models;
+using TravelBuddyAPI.Services.Interfaces;
+
+namespace TravelBuddyAPI.Services
 {
-    public class TripService
+    public class TripService : ITripService
     {
+        public async Task<Trip> CreateTripAsync(Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteTripAsync(Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Trip>> GetTripsForUserAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateTripAsync(Trip trip)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,33 @@
-﻿namespace TravelBuddyAPI.Services
+﻿using TravelBuddyAPI.Models;
+using TravelBuddyAPI.Services.Interfaces;
+
+namespace TravelBuddyAPI.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public async Task<User> CreateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
